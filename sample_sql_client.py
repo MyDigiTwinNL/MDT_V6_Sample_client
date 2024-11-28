7717bf8f1268991628514139b96c4bd1bd90cf01
Hector Cadavid <hector.cadavid@gmail.com>
2024-11-28 17:24:56 +0100

    first commit

---
+++ sample_sql_client.py
@@ -0,0 +1,30 @@
+from vantage6.client import UserClient as Client
+
+# Note: we assume here the config.py you just created is in the current directory.
+# If it is not, then you need to make sure it can be found on your PYTHONPATH
+import config
+
+# Initialize the client object, and run the authentication
+client = Client(config.server_url, config.server_port, config.server_api,
+                log_level='debug')
+client.authenticate(config.username, config.password)
+
+# Optional: setup the encryption, if you have an organization_key
+client.setup_encryption(config.organization_key)
+
+input_ = {
+    'method': 'central_average',
+    'kwargs': {"column_name":"VALUE"}
+}
+
+average_task = client.task.create(
+   collaboration=2,
+   organizations=[7,3,4],
+   name="poc-cli-lifelines-central-avg-oct-4",
+   image="harbor2.vantage6.ai/demo/average",
+   description='',
+   input_=input_,
+   databases=[
+      {'label': 'lifelines', 'query': 'select VALUE FROM CREATININE'}
+   ]
+)
